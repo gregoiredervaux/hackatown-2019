457d5d2baa3e3e2655c715f874b044ddba5a32d2
gregoiredervaux <gregoire.dervaux@2019.icam.fr>
2019-01-19 17:30:18 -0500

    creation des components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
+import { OptionsComponent } from './options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
